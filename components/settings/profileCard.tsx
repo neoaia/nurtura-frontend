4f71460c8e4fad8400dd7b713ca5b68c7757b35c
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-18 04:18:31 +0800

    refactor: profileCard

--- components/settings/profileCard.tsx
+++ components/settings/profileCard.tsx
@@ -9,22 +9,22 @@ interface ProfileCardProps {
 
 export const ProfileCard: React.FC<ProfileCardProps> = ({ name, username, iconSource }) => {
   return (
-    <View className=" w-full bg-white rounded-[20px] p-6 mx-4 my-4 flex-row items-center shadow-md elevation-3">
+    <View className=" w-full bg-white rounded-2xl py-5 px-6 mx-4 my-4 flex-row items-center shadow-md elevation-3">
       
-      <View className="w-[60px] h-[60px] bg-[#E9F2D9] rounded-full justify-center items-center">
+      <View className="w-12 h-12 bg-[#E9F2D9] rounded-full justify-center items-center">
         <Image 
           source={iconSource}
-          className="w-8 h-8"
+          className="w-5 h-5"
           style={{ tintColor: "#86975A" }}
           resizeMode="contain"
         />
       </View>
 
       <View className="ml-5">
-        <Text className="text-[14px] text-[#919191] font-medium">
+        <Text className="text-sm text-grayText font-medium">
           {name}
         </Text>
-        <Text className="text-[18px] font-bold text-[#333] mt-1">
+        <Text className="text-base font-bold text-black mt-1">
           {username}
         </Text>
       </View>
