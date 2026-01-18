adec1bb216f4531891e7714b6730957336981dc5
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-19 05:57:20 +0800

    feat: addNewPlant1 page

--- app/(tabs)/(add_pages)/(addNewPlant)/addNewPlant1.tsx
+++ app/(tabs)/(add_pages)/(addNewPlant)/addNewPlant1.tsx
@@ -1,10 +1,31 @@
+import { typography } from "@/assets/fonts/Text";
+import Dropdown from "@/components/shared/dropdown";
 import React from "react";
 import { Text, View } from "react-native";
 
 const addNewPlant1 = () => {
   return (
-    <View>
-      <Text>addNewPlant1</Text>
+    <View className="flex-1 bg-white px-[16px] pb-[34px] w-full justify-between h-screen">
+      <View className="mt-[34px] flex-1 items-start">
+        <Text
+          style={typography["h1-bold"]}
+          className="text-black mb-[13px] pl-2"
+        >
+          Select a{" "}
+          <Text style={typography["h1-bold"]} className="text-primary">
+            Nurtura Rack
+          </Text>
+        </Text>
+
+        <Text
+          style={typography["subheader"]}
+          className="mb-[20px] text-gray-700 leading-normal pl-2"
+        >
+          Enter a secure password to protect your account.
+        </Text>
+
+        <Dropdown></Dropdown>
+      </View>
     </View>
   );
 };
