6b86b7b786ee0a2f59cf298a915a8043ade7712f
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-18 15:21:05 +0800

    feat: added copy-able style properties

--- assets/fonts/Text.tsx
+++ assets/fonts/Text.tsx
@@ -1,72 +1,72 @@
 import { TextStyle } from "react-native";
 
 export const typography = {
-  title: {
+  title: { // style={typography['title']}
     fontSize: 40,
     lineHeight: 48,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "title-bold": {
+  "title-bold": { // style={typography['title-bold']}
     fontSize: 40,
     lineHeight: 48,
     fontFamily: "BricolageGrotesque-Bold",
   } as TextStyle,
-  h1: {
+  h1: { // style={typography['h1']}
     fontSize: 24,
     lineHeight: 29,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "h1-bold": {
+  "h1-bold": { // style={typography['h1-bold']}
     fontSize: 24,
     lineHeight: 29,
     fontFamily: "BricolageGrotesque-ExtraBold",
   } as TextStyle,
-  h2: {
+  h2: { // style={typography['h2-bold']}
     fontSize: 18,
     lineHeight: 22,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "h2-bold": {
+  "h2-bold": { // style={typography['h2-bold']}
     fontSize: 18,
     lineHeight: 22,
     fontFamily: "BricolageGrotesque-Bold",
   } as TextStyle,
-  subheader: {
+  subheader: { // style={typography['subheader']}
     fontSize: 14,
     lineHeight: 23,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "subheader-bold": {
+  "subheader-bold": { // style={typography['subheader-bold']}
     fontSize: 14,
     lineHeight: 23,
     fontFamily: "BricolageGrotesque-Bold",
   } as TextStyle,
-  label: {
+  label: { // style={typography['label']}
     fontSize: 13,
     lineHeight: 16,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "label-bold": {
+  "label-bold": { // style={typography['label-bold']}
     fontSize: 13,
     lineHeight: 16,
     fontFamily: "BricolageGrotesque-Bold",
   } as TextStyle,
-  body: {
+  body: { // style={typography['body']}
     fontSize: 11,
     lineHeight: 14,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "body-bold": {
+  "body-bold": { // style={typography['body-bold']}
     fontSize: 11,
     lineHeight: 14,
     fontFamily: "BricolageGrotesque-Bold",
   } as TextStyle,
-  button: {
+  button: { // style={typography['button']}
     fontSize: 16,
     lineHeight: 20,
     fontFamily: "BricolageGrotesque-Regular",
   } as TextStyle,
-  "button-bold": {
+  "button-bold": { // style={typography['button-bold']}
     fontSize: 16,
     lineHeight: 20,
     fontFamily: "BricolageGrotesque-Bold",
