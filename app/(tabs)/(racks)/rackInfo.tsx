6fb56b88ba9fe1499e08a6ed0ac67d0b405efb96
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-19 19:13:54 +0800

    fix: rackPages hide bottom nav

--- app/(tabs)/(racks)/rackInfo.tsx
+++ app/(tabs)/(racks)/rackInfo.tsx
@@ -2,18 +2,38 @@ import { typography } from "@/assets/fonts/Text";
 import PlantStatusIndicators from "@/components/racks/plantStatusIndicators";
 import { MenuCard } from "@/components/shared/menubtn";
 import SmallDescription from "@/components/shared/smallDescription";
-import React from "react";
+import { useNavigation } from "expo-router";
+import React, { useLayoutEffect } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const rackInfo = () => {
+const RackInfo = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.getParent()?.setOptions({
+      tabBarStyle: { display: "none" },
+    });
+
+    return () => {
+      navigation.getParent()?.setOptions({
+        tabBarStyle: {
+          height: 100,
+          paddingBottom: 10,
+          paddingTop: 15,
+          display: "flex",
+        },
+      });
+    };
+  }, [navigation]);
+
   return (
     <SafeAreaView className="bg-white flex-1">
       <ScrollView showsVerticalScrollIndicator={false}>
         <View className="px-4 py-4 bg-white">
           <View className="w-full flex-row justify-between items-start mb-6">
             <View className="flex-1 pl-2">
-              <Text style={typography["h1-bold"]} className="  text-black">
+              <Text style={typography["h1-bold"]} className="text-black">
                 Lettuce
               </Text>
               <Text style={typography["subheader"]} className="text-grayText">
@@ -21,7 +41,7 @@ const rackInfo = () => {
               </Text>
             </View>
             <View className="items-end pr-2">
-              <Text style={typography["h1-bold"]} className="  text-black">
+              <Text style={typography["h1-bold"]} className="text-black">
                 3
               </Text>
               <Text style={typography["subheader"]} className="text-grayText">
@@ -49,24 +69,23 @@ const rackInfo = () => {
           </View>
           <View className="flex-col gap-3 mb-8">
             <MenuCard
-            title="Plant Care Activity"
-            description="Logs based on watering and grow light activity."
-            iconSource={require("@/assets/images/plantcare-icon.png")}
-            route="/(tabs)/(racks)/care"
-          ></MenuCard>
+              title="Plant Care Activity"
+              description="Logs based on watering and grow light activity."
+              iconSource={require("@/assets/images/plantcare-icon.png")}
+              route="/(tabs)/(racks)/care"
+            />
 
-          <MenuCard
-            title="Harvest Activity"
-            description="Records of your past harvests for this plant."
-            iconSource={require("@/assets/images/harvest-icon.png")}
-            route="/(tabs)/(racks)/harvestHistory"
-          ></MenuCard>
+            <MenuCard
+              title="Harvest Activity"
+              description="Records of your past harvests for this plant."
+              iconSource={require("@/assets/images/harvest-icon.png")}
+              route="/(tabs)/(racks)/harvestHistory"
+            />
           </View>
-          
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default rackInfo;
+export default RackInfo;
