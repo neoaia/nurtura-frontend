4a83c990e4fc1ec0ddc22d4745f6020d3b5c0526
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-19 19:33:21 +0800

    fix: home pages hide bottom nav

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,10 +1,30 @@
-import { Stack } from 'expo-router';
+import { typography } from "@/assets/fonts/Text";
+import { Stack } from "expo-router";
+import { TextStyle } from "react-native";
 
 export default function HomeLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="notification" />
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#fafafa" },
+        headerBlurEffect: "light",
+        headerBackTitle: "Back",
+        headerShadowVisible: false,
+        headerTitleAlign: "center",
+        headerShown: true,
+        headerTitleStyle: {
+          ...(typography["h2-bold"] as TextStyle),
+          color: "#424242",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="notification" options={{ title: "Notifications" }} />
     </Stack>
   );
 }
