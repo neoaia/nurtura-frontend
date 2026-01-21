f0e66733f11e22a8c0a305ff46bed5848cb7e3fc
neoaia <151616379+neoaia@users.noreply.github.com>
2026-01-21 23:08:07 +0800

    feat: dark notif text

--- app/(tabs)/(account)/_layout.tsx
+++ app/(tabs)/(account)/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function AccountLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="account"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="account"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
--- app/(tabs)/(racks)/_layout.tsx
+++ app/(tabs)/(racks)/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RackLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="racks"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="racks"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
