c8887fd3573300f6ee73627de662e02d2c3e7821
David De Jesus <dejesus.davidjim@gmail.com>
2026-02-12 16:47:56 +0800

    refactor: change user info button and add address fields

--- app/(tabs)/(account)/index.tsx
+++ app/(tabs)/(account)/index.tsx
@@ -3,20 +3,17 @@ import { LogOutRow } from "@/components/settings/logoutTab";
 import { ProfileCard } from "@/components/settings/profileCard";
 import { SettingsRow } from "@/components/settings/settingsTab";
 
-import React from "react";
-import { ScrollView, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "expo-router";
+import React from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AccountScreen() {
   const { logout } = useAuth();
+  const router = useRouter();
 
   const menuItems = [
-    {
-      title: "User Information",
-      icon: require("@/assets/images/user-info-icon.png"),
-      path: "/(tabs)/(account)/user-info",
-    },
     {
       title: "Account Security",
       icon: require("@/assets/images/security-icon.png"),
@@ -37,11 +34,16 @@ export default function AccountScreen() {
             </Text>
           </View>
           <View className="mb-6 w-full">
-            <ProfileCard
-              name="Juan Dela Cruz"
-              username="JuanMasipag"
-              iconSource={require("@/assets/images/user-icon-settings.png")}
-            />
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => router.push("/(tabs)/(account)/user-info")}
+            >
+              <ProfileCard
+                name="Juan Dela Cruz"
+                username="JuanMasipag"
+                iconSource={require("@/assets/images/user-icon-settings.png")}
+              />
+            </TouchableOpacity>
           </View>
 
           <View className="flex justify-center items-center bg-white">
--- app/(tabs)/(account)/user-info.tsx
+++ app/(tabs)/(account)/user-info.tsx
@@ -1,41 +1,171 @@
-import React, { useState } from "react";
-import { ScrollView, View } from "react-native";
+import React, { useMemo, useState } from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
+import { typography } from "@/assets/fonts/Text";
+import CloseIcon from "@/assets/images/icons/closeIcon.svg";
+import EditIcon from "@/assets/images/icons/editIcon.svg";
+import { PrimaryButton } from "@/components/shared/primaryButton";
 import { TextInputField } from "@/components/shared/textInputField";
 
 export default function UserInformationScreen() {
-  const [username, setUsername] = useState("JuanMasipag");
-  const [firstName, setFirstName] = useState("Juan");
-  const [lastName, setLastName] = useState("Dela Cruz");
+  const [savedValues, setSavedValues] = useState({
+    firstName: "Juan",
+    middleName: "",
+    lastName: "Dela Cruz",
+    suffix: "",
+    block: "",
+    street: "",
+    barangay: "",
+    city: "",
+  });
+
+  const [formValues, setFormValues] = useState(savedValues);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const hasChanges = useMemo(() => {
+    return Object.keys(savedValues).some(
+      (key) =>
+        savedValues[key as keyof typeof savedValues] !==
+        formValues[key as keyof typeof formValues],
+    );
+  }, [formValues, savedValues]);
+
+  const handleChange = (field: keyof typeof formValues, value: string) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  const handleToggleEdit = () => {
+    if (isEditing) {
+      setFormValues(savedValues);
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    }
+  };
+
+  const handleSubmitUserInfo = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setSavedValues(formValues);
+      setIsEditing(false);
+      setLoading(false);
+      console.log("Updated!");
+    }, 1000);
+  };
 
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 20 }}
-      className="bg-white"
-    >
-      <View className="px-6 mt-6">
+    <View className="flex-1 bg-white">
+      <View className="flex-row justify-end px-6 pt-6">
+        <TouchableOpacity onPress={handleToggleEdit}>
+          {isEditing ? (
+            <CloseIcon width={22} height={22} />
+          ) : (
+            <EditIcon width={22} height={22} />
+          )}
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+        className="px-6 pt-4"
+      >
         <TextInputField
-          label="Username"
-          value={username}
-          onChangeText={setUsername}
+          label="First Name"
+          value={formValues.firstName}
+          onChangeText={(text) => handleChange("firstName", text)}
+          editable={isEditing}
         />
 
-        <View className="flex-row justify-between">
-          <TextInputField
-            label="First name"
-            value={firstName}
-            onChangeText={setFirstName}
-            width="w-[48%]"
-          />
-          <TextInputField
-            label="Last Name"
-            value={lastName}
-            onChangeText={setLastName}
-            width="w-[48%]"
+        <TextInputField
+          label="Middle Name (optional)"
+          value={formValues.middleName}
+          onChangeText={(text) => handleChange("middleName", text)}
+          editable={isEditing}
+        />
+
+        <View className="flex-row gap-3">
+          <View className="flex-1">
+            <TextInputField
+              label="Last Name"
+              value={formValues.lastName}
+              onChangeText={(text) => handleChange("lastName", text)}
+              editable={isEditing}
+            />
+          </View>
+
+          <View className="w-[100px]">
+            <TextInputField
+              label="Suffix"
+              value={formValues.suffix}
+              onChangeText={(text) => handleChange("suffix", text)}
+              editable={isEditing}
+            />
+          </View>
+        </View>
+
+        <Text
+          style={typography["button-bold"]}
+          className="text-black tracking-wide mb-3 mt-6"
+        >
+          Address
+        </Text>
+
+        <View className="flex-row gap-3 mb-1">
+          <View className="w-[100px]">
+            <TextInputField
+              label="Block/No."
+              value={formValues.block}
+              onChangeText={(text) => handleChange("block", text)}
+              editable={isEditing}
+            />
+          </View>
+
+          <View className="flex-1">
+            <TextInputField
+              label="Street"
+              value={formValues.street}
+              onChangeText={(text) => handleChange("street", text)}
+              editable={isEditing}
+            />
+          </View>
+        </View>
+
+        <View className="flex-row gap-3">
+          <View className="flex-1">
+            <TextInputField
+              label="Barangay"
+              value={formValues.barangay}
+              onChangeText={(text) => handleChange("barangay", text)}
+              editable={isEditing}
+            />
+          </View>
+
+          <View className="flex-1">
+            <TextInputField
+              label="City"
+              value={formValues.city}
+              onChangeText={(text) => handleChange("city", text)}
+              editable={isEditing}
+            />
+          </View>
+        </View>
+      </ScrollView>
+
+      {isEditing && hasChanges && (
+        <View className="px-4 pb-20  bg-white border-t border-gray-100">
+          <PrimaryButton
+            onPress={handleSubmitUserInfo}
+            loading={loading}
+            title="Save Changes"
           />
         </View>
-      </View>
-    </ScrollView>
+      )}
+    </View>
   );
 }
